dece3ff911c4a9c03a35f5d1cbb61a62531a2609
hongocan <hongocan792002>
2022-08-03 15:31:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react'
+import {listQs} from './listQs'
 function App() {
+  const [number, setNumber] = useState(0)
+  
+  const randomNumber = () => {
+    setNumber(number => number =Math.floor(Math.random() * 90))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <div className="App">
+          <div className="Container">
+            <h2 className="question">{listQs[number].qs}</h2>
+            <button className="button" onClick={randomNumber}>
+              Click
+            </button>
+          </div>
+        </div>
+    </>
   );
 }
 
---
+++ src/listQs.js
@@ -0,0 +1,362 @@
+export const listQs = [
+    {
+        id: 0,
+        qs: 'Chọn Câu Hỏi'
+    },
+    {
+        id: 1,
+        qs: 'Câu 1: Khái niệm và đặc điểm của Logistics?'
+    },
+    {
+        id: 2,
+        qs: 'Câu 2: Vai trò của Logistics?'
+    },
+    {
+        id: 3,
+        qs: 'Câu 3: Phân loại Logistics?'
+    },
+    {
+        id: 4,
+        qs: 'Câu 4: Những nhân tố ảnh hưởng đến sự hình thành và phát triển của Logisitcs?'
+    },
+    {
+        id: 5,
+        qs: 'Câu 5: Phân tích mối quan hệ giữa vận tải và buôn bán quốc tế'
+    },
+    {
+        id: 6,
+        qs: 'Câu 6: Quyền vận tải là gì? Trình bày cơ sở phân chia quyền vận tải trong ngoại thương?'
+    },
+    {
+        id: 7,
+        qs: 'Câu 7: Các điều kiện cơ sở giao hàng mà người bán giành được quyền vận tải theo Incoterms 2020'
+    },
+    {
+        id: 8,
+        qs: 'Câu 8: Các điều kiện cơ sở giao hàng mà người mua giành được quyền vận tải theo Incoterms 2020'
+    },
+    {
+        id: 9,
+        qs: 'Câu 9: Phân tích những lợi ích khi giành được quyền vận tải'
+    },
+    {
+        id: 10,
+        qs: 'Câu 10: Phân tích những trường hợp không nên giành quyền vận tải'
+    },
+    {
+        id: 11,
+        qs: 'Câu 11: Ưu nhược điểm của vận tải biển?'
+    },
+    {
+        id: 12,
+        qs: 'Câu 12: Vai trò của vận tải biển đối với buôn bán quốc tế'
+    },
+    {
+        id: 13,
+        qs: 'Câu 13: Nêu các trang thiết bị cơ bản của một cảng biển'
+    },
+    {
+        id: 14,
+        qs: 'Câu 14: Khái niệm tầu buôn và các cách phân loại tàu buôn'
+    },
+    {
+        id: 15,
+        qs: 'Câu 15: Đặc trưng kinh tế kỹ thuật tầu buôn'
+    },
+    {
+        id: 16,
+        qs: 'Câu 16: Mớn nước của tàu: khái niệm và ý nghĩa?'
+    },
+    {
+        id: 17,
+        qs: 'Câu 17: Cờ tàu là gì? Ý nghĩa của việc cắm cờ thường và cắm cờ phương tiện'
+    },
+    {
+        id: 18,
+        qs: 'Câu 18: Khái niệm và ý nghĩa của hệ số xếp hàng của hàng và hệ số xếp hàng của tàu'
+    },
+    {
+        id: 19,
+        qs: 'Câu 19: Khái niệm, đặc điểm và phương thức thuê tàu chợ'
+    },
+    {
+        id: 20,
+        qs: 'Câu 20: Trình bày khái niệm của phương thức thuê tàu chợ và trình tự các bước thuê tàu.'
+    },
+    {
+        id: 21,
+        qs: 'Câu 21: Khái niệm và các chức năng vận đơn đường biển'
+    },
+    {
+        id: 22,
+        qs: 'Câu 22: Phân biệt giữa vận đơn đã xếp hàng lên tàu và vận đơn nhận hàng để xếp.'
+    },
+    {
+        id: 23,
+        qs: 'Câu 23: Phân biệt vận đơn đích danh, vận đơn theo lệnh, vận đơn vô danh.'
+    },
+    {
+        id: 24,
+        qs: 'Câu 24: Ký hậu chuyển nhượng chứng từ vận tải là gì ? Có những cách ký hậu chuyển nhượng nào ?'
+    },
+    {
+        id: 25,
+        qs: 'Câu 25: Switch B/L là gì ?'
+    },
+    {
+        id: 26,
+        qs: 'Câu 26: Trình bày về Surrendered Bill of Lading và Sea Way Bill.'
+    },
+    {
+        id: 27,
+        qs: 'Câu 27: Trình bày trách nhiệm của người chuyên chở đường biển đối với hàng hóa theo Quy tắc Hague.'
+    },
+    {
+        id: 28,
+        qs: 'Câu 28: Trình bày trách nhiệm của người chuyên chở đường biển đối với hàng hóa theo Quy tắc Hague-Visby.'
+    },
+    {
+        id: 29,
+        qs: 'Câu 29: Trình bày trách nhiệm của người chuyên chở đường biển đối với hàng hóa theo Quy tắc Hamburg.'
+    },
+    {
+        id: 30,
+        qs: 'Câu 30: Trình bày các nguồn luật điều chỉnh trách nhiệm của người chuyên chở đường biển đối với hàng hóa vận chuyển theo vận đơn và so sánh nội dung về thời hạn trách nhiệm của người chuyên chở theo các nguồn luật đó.'
+    },
+    {
+        id: 31,
+        qs: 'Câu 31: Trình bày các nguồn luật điều chỉnh trách nhiệm của người chuyên chở đường biển đối với hàng hóa vận chuyển theo vận đơn và so sánh nội dung về cơ sở trách nhiệm của người chuyên chở theo các nguồn luật đó.'
+    },
+    {
+        id: 32,
+        qs: 'Câu 32: Trình bày các nguồn luật điều chỉnh trách nhiệm của người chuyên chở đường biển đối với hàng hóa vận chuyển theo vận đơn và so sánh nội dung về giới hạn trách nhiệm của người chuyên chở theo các nguồn luật đó.'
+    },
+    {
+        id: 33,
+        qs: 'Câu 33: Trình bày nội dung về thông báo tổn thất và khiếu nại người chuyên chở đường biển đối với hàng hóa vận chuyển theo vận đơn theo các nguồn luật quốc tế hiện hành (Quy tắc Hague, Quy tắc Hague-Visby, Quy tắc Hamburg)'
+    },
+    {
+        id: 34,
+        qs: 'Câu 34: Trình bày trách nhiệm của người chuyên chở đường biển đối với hàng hóa vận chuyển theo vận đơn theo Bộ luật Hàng hải Việt Nam 2015.'
+    },
+    {
+        id: 35,
+        qs: 'Câu 35: Khái niệm và đặc điểm của tàu chuyến'
+    },
+    {
+        id: 36,
+        qs: 'Câu 36: Khái niệm phương thức thuê tàu chuyến, trình tự các bước thuê tàu chuyến và các hình thức thuê tàu chuyến'
+    },
+    {
+        id: 37,
+        qs: 'Câu 37: Hãy quy định các điều khoản: cảng xếp dỡ, thời gian xếp dỡ, để chuyên chở 1 lô hàng 10.000 tấn gạo từ Việt Nam ra nước ngoài'
+    },
+    {
+        id: 38,
+        qs: 'Câu 38: Quy định về tàu của hợp đồng thuê tàu chuyến'
+    },
+    {
+        id: 39,
+        qs: 'Câu 39: Quy định về thời gian tàu đến cảng xếp hàng của hợp đồng thuê tàu chuyến'
+    },
+    {
+        id: 40,
+        qs: 'Câu 40: Quy định về hàng hóa của hợp đồng thuê tàu chuyến'
+    },
+    {
+        id: 41,
+        qs: 'Câu 41: Quy định về cảng xếp dỡ của hợp đồng thuê tàu chuyến'
+    },
+    {
+        id: 42,
+        qs: 'Câu 42: Quy định về chi phí xếp dỡ của hợp đồng thuê tàu chuyến'
+    },
+    {
+        id: 43,
+        qs: 'Câu 43: Quy định về cước phí thuê tàu của hợp đồng thuê tàu chuyến'
+    },
+    {
+        id: 44,
+        qs: 'Câu 44: Trình bày điều khoản quy định về thưởng/phạt xếp dỡ trong hợp đồng thuê tàu chuyến.'
+    },
+    {
+        id: 45,
+        qs: 'Câu 45: Khái niệm, đặc điểm, các hình thức thuê tàu định hạn và các trường hợp áp dụng thuê tàu định hạn.'
+    },
+    {
+        id: 46,
+        qs: 'Câu 46: Hãy quy định các điều khoản: thời gian xếp dỡ, thưởng phạt xếp dỡ để chuyên chở 1 lô hàng 10.000 tấn gạo từ Việt Nam ra nước ngoài'
+    },
+    {
+        id: 47,
+        qs: 'Câu 47: Hãy quy định các điều khoản: cước phí, luật lệ giải quyết tranh chấp, để chuyên chở 1 lô hàng 10.000 tấn gạo từ Việt Nam ra nước ngoài'
+    },
+    {
+        id: 48,
+        qs: 'Câu 48: Hãy quy định các điều khoản: hàng hóa, con tàu, để chuyên chở 1 lô hàng 10.000 tấn gạo từ Việt Nam ra nước ngoài'
+    },
+    {
+        id: 49,
+        qs: 'Câu 49: Hãy quy định các điều khoản: chi phí xếp dỡ, thời gian tàu đến cảng xếp hàng, để chuyên chở 1 lô hàng 10.000 tấn gạo từ Việt Nam ra nước ngoài'
+    },
+    {
+        id: 50,
+        qs: 'Câu 50: Phân biệt B/L hoàn hảo và không hoàn hảo'
+    },
+    {
+        id: 51,
+        qs: 'Câu 51: So sánh cơ sở trách nhiệm của người chuyên chở đường  biển theo Quy tắc Hague, Quy tắc Hague-Visby và Quy tắc Hamburg.'
+    },
+    {
+        id: 52,
+        qs: 'Câu 52: So sánh thời trách nhiệm của người chuyên chở đường biển theo Quy tắc Hague, Quy tắc Hague-Visby và Quy tắc Hamburg.'
+    },
+    {
+        id: 53,
+        qs: 'Câu 53: So sánh giới hạn trách nhiệm của người chuyên chở đường biển theo Quy tắc Hague, Quy tắc Hague-Visby và Quy tắc Hamburg.'
+    },
+    {
+        id: 54,
+        qs: 'Câu 54: So sánh phương thức thuê tàu chợ và phương thức thuê tàu chuyến'
+    },
+    {
+        id: 55,
+        qs: 'Câu 55: Vị trí và đặc điểm của vận tải hàng không.'
+    },
+    {
+        id: 56,
+        qs: 'Câu 56: ULD là gì? Trình bày các loại ULD?'
+    },
+    {
+        id: 57,
+        qs: 'Câu 57: Các tổ chức vận tải hàng không quốc tế và Việt Nam.'
+    },
+    {
+        id: 58,
+        qs: 'Vui lòng random lại'
+    },
+    {
+        id: 59,
+        qs: 'Câu 59: Trình bày quy trình gửi hàng hóa bằng đường hàng không qua đại lý hàng hóa hàng không?'
+    },
+    {
+        id: 60,
+        qs: 'Câu 60: Trình bày quy trình gửi hàng hóa bằng đường hàng không qua người giao nhận hàng không?'
+    },
+    {
+        id: 61,
+        qs: 'Câu 61: Vận đơn hàng không là gì? Nêu các loại vận đơn hàng không và trường hợp sử dụng chúng. Trình bày cách lập và phân phối vận đơn hàng không.'
+    },
+    {
+        id: 62,
+        qs: 'Câu 62: Các chức năng của AWB'
+    },
+    {
+        id: 63,
+        qs: 'Câu 63: Cước hàng không là gì? Trình bày các loại cước hàng không.'
+    },
+    {
+        id: 64,
+        qs: 'Câu 64: Lợi ích của việc vận chuyển hàng hóa bằng container (đối với người chuyên chở, gom hàng, người gửi hàng)'
+    },
+    {
+        id: 65,
+        qs: 'Câu 65: Container là gì? Container được tiêu chuẩn hóa như thế nào? Phân loại container.'
+    },
+    {
+        id: 66,
+        qs: 'Câu 66: Trình bày về cảng, ga, bến bãi container.'
+    },
+    {
+        id: 67,
+        qs: 'Câu 67: Phương thức gửi hàng nguyên container FCL/FCL'
+    },
+    {
+        id: 68,
+        qs: 'Câu 68: Phương thức gửi hàng lẻ container LCL/LCL'
+    },
+    {
+        id: 69,
+        qs: 'Câu 69: Phương thức gửi hàng kết hợp FCL/LCL & LCL/FCL'
+    },
+    {
+        id: 70,
+        qs: 'Câu 70: Khái niệm cước phí vận chuyển container, các bộ phận cấu thành và các yếu tố ảnh hưởng'
+    },
+    {
+        id: 71,
+        qs: 'Câu 71: Chứng từ trong vận chuyển hàng hóa bằng container? Phân loại và chức năng?'
+    },
+    {
+        id: 72,
+        qs: 'Câu 72: Trình bày trách nhiệm của người chuyên chở container đối với hàng hóa'
+    },
+    {
+        id: 73,
+        qs: 'Câu 73: Dịch vụ gom hàng là gì và lợi ích của nó? Trách nhiệm và vai trò của của người gom hàng'
+    },
+    {
+        id: 74,
+        qs: 'Câu 74: Dịch vụ gom hàng là gì? Các bước trong nghiệp vụ gom hàng'
+    },
+    {
+        id: 75,
+        qs: 'Câu 75: Phân biệt Master B/L và House B/L'
+    },
+    {
+        id: 76,
+        qs: 'Câu 76: Tại sao khi gửi hàng bằng container nên thay các điều kiện Incoterms CIF, FOB, CFR bằng các điều kiện CIP, FCA, CPT'
+    },
+    {
+        id: 78,
+        qs: 'Câu 78: Định nghĩa và đặc điểm VTĐPT'
+    },
+    {
+        id: 79,
+        qs: 'Câu 79: Các hình thức tổ chức VTĐPT'
+    },
+    {
+        id: 80,
+        qs: 'Câu 80: Vận tải đa phương thức là gì? Hiệu quả của VTĐPT'
+    },
+    {
+        id: 81,
+        qs: 'Câu 81: Nêu các nguồn luật điều chỉnh vận tải đa phương thức trên thế giới và ở Việt Nam.'
+    },
+    {
+        id: 82,
+        qs: 'Câu 82: Định nghĩa và phân loại MTO'
+    },
+    {
+        id: 83,
+        qs: 'Câu 83: Chế độ trách nhiệm thống nhất là gì? Phân biệt chế độ trách nhiệm thống nhất và chế độ trách nhiệm từng chặng'
+    },
+    {
+        id: 84,
+        qs: 'Câu 84: Quy định thời hạn trách nhiệm của MTO'
+    },
+    {
+        id: 85,
+        qs: 'Câu 85: Quy định cơ sở trách nhiệm của MTO'
+    },
+    {
+        id: 86,
+        qs: 'Câu 86: Quy định giới hạn trách nhiệm của MTO'
+    },
+    {
+        id: 87,
+        qs: 'Câu 87: So sánh trách nhiệm của MTO theo 3 nguồn luật điều chỉnh VTĐPT: CƯ 1980 của Liên Hợp Quốc, Bản Quy tắc UNTACD/ICC và NĐ 87/2009/NĐ-CP.'
+    },
+    {
+        id: 88,
+        qs: 'Câu 88: Quy định về thông báo tổn thất và khiếu nại người kinh doanh vận tải đa phương thức'
+    },
+    {
+        id: 89,
+        qs: 'Câu 89:  Định nghĩa, các loại chứng từ vận tải đa phương thức'
+    },
+    {
+        id: 90,
+        qs: 'Câu 90: Ưu nhược điểm của VTĐPT'
+    },
+];
